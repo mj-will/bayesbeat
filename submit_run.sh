c7ea034c03b52a8bfc29d538f49ab6d5ef477f45
mj-will <m.williams.4@research.gla.ac.uk>
2023-02-02 15:01:00 +0000

    Update condor files

--- submit_run.sh
+++ submit_run.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-index=$1
+datafile=$1
+index=$2
+outdir=$3
+sed -i "/datafile = */c\datafile = $datafile" bayes_beat.submit
 sed -i "/index = */c\index = $index" bayes_beat.submit
+sed -i "/outdir = */c\outdir = $outdir" bayes_beat.submit
 condor_submit bayes_beat.submit
