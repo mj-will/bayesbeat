b3cb041cf8fa1d1fc0583a529e5ca00581bea373
mj-will <m.williams.4@research.gla.ac.uk>
2023-06-01 17:41:19 +0100

    feat: add create_ini to cli

--- src/bayesbeat/cli/config.py
+++ src/bayesbeat/cli/config.py
@@ -7,3 +7,17 @@ from ..config.config import BayesBeatConfigParser
 def base_config(name):
     config = BayesBeatConfigParser()
     config.write_to_file(name)
+
+
+
+@click.command()
+@click.option("--output", type=str)
+@click.option("--datafile", type=str)
+@click.option("--label", type=str)
+@click.option("--filename", type=str)
+def create_ini(filename, output, datafile, label):
+    config = BayesBeatConfigParser()
+    config.set("General", "output", output)
+    config.set("General", "datafile", datafile)
+    config.set("General", "label", label)
+    config.write_to_file(filename)
