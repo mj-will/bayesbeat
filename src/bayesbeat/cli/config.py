45b263195ebf2e59d9535e33f8ca7719042857cb
mj-will <m.williams.4@research.gla.ac.uk>
2023-07-24 12:06:09 +0100

    refactor!: use arguments for config file input

--- src/bayesbeat/cli/config.py
+++ src/bayesbeat/cli/config.py
@@ -2,20 +2,37 @@ import click
 
 from ..config.config import BayesBeatConfigParser
 
+
 @click.command()
-@click.argument("name")
-def base_config(name):
+@click.argument("filename", type=click.Path())
+def base_config(filename):
     config = BayesBeatConfigParser()
-    config.write_to_file(name)
-
+    config.write_to_file(filename)
 
 
 @click.command()
-@click.option("--output", type=click.Path())
-@click.option("--datafile", type=click.Path(exists=True))
-@click.option("--label", type=str)
-@click.option("--filename", type=str)
+@click.argument(
+    "filename",
+    type=click.Path(),
+)
+@click.option(
+    "--output", type=click.Path(), default=None, help="Output directory."
+)
+@click.option(
+    "--datafile",
+    type=click.Path(exists=True),
+    default=None,
+    help="Data file to analyze.",
+)
+@click.option(
+    "--label", type=str, default=None, help="Label for the analysis."
+)
 def create_ini(filename, output, datafile, label):
+    """Create a default ini file with a given filename.
+
+    Allows for some settings in the ini file to be set directly from the command
+    line.
+    """
     config = BayesBeatConfigParser()
     config.set("General", "output", output)
     config.set("General", "datafile", datafile)
