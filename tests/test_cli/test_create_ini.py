37933110b4d3dacfba2083578429d406c4788b0e
mj-will <m.williams.4@research.gla.ac.uk>
2023-07-26 12:49:20 +0100

    test: add basic test for `create_ini`

---
+++ tests/test_cli/test_create_ini.py
@@ -0,0 +1,10 @@
+"""Tests for the create ini CLI"""
+from click.testing import CliRunner
+
+from bayesbeat.cli.config import create_ini
+
+
+def test_basic_usage():
+    runner = CliRunner()
+    result = runner.invoke(create_ini, "test.ini")
+    assert result.exit_code == 0
