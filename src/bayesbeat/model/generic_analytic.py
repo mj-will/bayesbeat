79945c17fb574dbaf302312f33bd52267c8a8ef0
mj-will <michaeljw1@googlemail.com>
2024-05-14 17:34:11 +0100

    rework model to use files directly

--- src/bayesbeat/model/generic_analytic.py
+++ src/bayesbeat/model/generic_analytic.py
@@ -1,4 +1,5 @@
 """Analytic model derived by Bryan Barr"""
+import logging
 from typing import Optional
 
 import inspect
@@ -16,6 +17,39 @@ except ImportError:
 
     def jit(*args, **kwargs):
         return lambda f: f
+    
+logger = logging.getLogger(__name__)
+
+
+def convert_to_dw(expression):
+    """Convert the expression to use dw instead of w_1 - w_2"""
+    expression = expression.replace("w_1 - w_2", "dw")
+    expression = expression.replace("2*w_1 - 2*w_2", "2*dw")
+    expression = expression.replace("3*w_1 - 3*w_2", "3*dw")
+    return expression
+
+
+def read_function_from_sympy_file(equation_filename):
+    """Read a function from a txt file using sympy.
+    
+    Returns
+    -------
+    Callable : 
+        The lambdified function
+    """
+    from sympy import lambdify
+    from sympy.parsing.sympy_parser import parse_expr
+
+    with open(equation_filename, "r") as f:
+        expression = f.readline()
+
+    expression = convert_to_dw(expression)
+    logger.debug(f"Parsing expression: {expression}")
+    func = parse_expr(expression)
+    variables = sorted(func.free_symbols, key=lambda s: s.name)
+    logger.debug(f"Found the following variables: {variables}")
+    func_lambdify = lambdify(variables, func)
+    return func_lambdify
 
 
 class GenericAnalyticGaussianBeam(UniformPriorMixin, BaseModel):
@@ -40,7 +74,7 @@ class GenericAnalyticGaussianBeam(UniformPriorMixin, BaseModel):
         rescale: bool = False,
         decay_constraint: bool = False,
         amplitude_constraint: bool = False,
-        n_terms: int = 3,
+        equation_filename: str = None,
         coefficients_filename: str = None,
         **kwargs
     ) -> None:
@@ -51,12 +85,14 @@ class GenericAnalyticGaussianBeam(UniformPriorMixin, BaseModel):
         self.amplitude_constraint = amplitude_constraint
         self.decay_constraint = decay_constraint
 
-        if n_terms != 3:
-            raise Exception("Currently only working for three terms")
-
-        terms_filename = coefficients_filename
-        with open(terms_filename, "rb") as f:
-            self.coefficients = dill.load(f, "rb")
+        with open(coefficients_filename, "rb") as f:
+            coefficients = dill.load(f, "rb")
+        self.coefficients = {
+            f"C_{i}": c for i, c in enumerate(coefficients)
+        }
+        
+        func = read_function_from_sympy_file(equation_filename)
+        self.func = jit(func, nopython=True)
 
         if rescale is True:
             raise NotImplementedError
@@ -101,8 +137,8 @@ class GenericAnalyticGaussianBeam(UniformPriorMixin, BaseModel):
     @property
     def model_parameters(self) -> list[str]:
         if self._model_parameters is None:
-            params = set(inspect.signature(signal_model).parameters.keys())
-            self._model_parameters = params - {"x_data"}
+            params = set(inspect.signature(self.model_function).parameters.keys())
+            self._model_parameters = params
         return self._model_parameters
 
     def evaluate_constraints(self, x):
@@ -128,7 +164,7 @@ class GenericAnalyticGaussianBeam(UniformPriorMixin, BaseModel):
         x = live_points_to_dict(x, self.names)
         x.update(self.constant_parameters)
         sigma_noise = x.pop("sigma_noise")
-        y_signal = signal_model(self.x_data, self.coefficients, **x)
+        y_signal = self.model_function(**x)
 
         norm_const = (
             -0.5 * self.n_samples * np.log(2 * np.pi * sigma_noise**2)
@@ -143,296 +179,28 @@ class GenericAnalyticGaussianBeam(UniformPriorMixin, BaseModel):
         x = live_points_to_dict(x, self.names)
         x.update(self.constant_parameters)
         x.pop("sigma_noise")
-        return signal_model(self.x_data, self.coefficients, **x)
-
-
-def signal_model(
-    x_data: np.ndarray,
-    Cterms: np.ndarray,
-    a_1: float,
-    a_2: float,
-    a_scale: float,
-    tau_1: float,
-    tau_2: float,
-    domega: float,
-    dphi: float,
-    x_offset: float,
-):
-    """Includes scaling and the square-root"""
-    return a_scale * np.sqrt(
-        model_function(
-            x_data,
-            Cterms,
-            a_1,
-            a_2,
-            tau_1,
-            tau_2,
-            domega,
-            dphi,
-            x_offset,
-        )
-    )
-
-
-def model_function(
-    x_data: np.ndarray,
-    Cterms: np.ndarray,
-    a_1: float,
-    a_2: float,
-    tau_1: float,
-    tau_2: float,
-    domega: float,
-    dphi: float,
-    x_offset: float,
-):
-    # Pre-calculate various elements
-    B1 = a_1 * np.exp(-x_data / tau_1)
-    B2 = a_2 * np.exp(-x_data / tau_2)
-    dw = x_data * domega + dphi
-    output = three_term_generic(B1, B2, dw, x_offset, Cterms)
-
-    return output
-
-
-@jit(nopython=True)
-def three_term_generic(
-    B_1: float, B_2: float, dw: float, x_0: float, _Dummy_36: np.ndarray
-):
-    """three term generic function
-
-    Args:
-        B_1 (_type_): _description_
-        B_2 (_type_): _description_
-        dw (_type_): _description_
-        x_0 (_type_): _description_
-        _Dummy_36 (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    [C_0, C_1, C_2, C_3] = _Dummy_36
-    output = (
-        (29 / 128) * B_1**6 * C_3**2
-        + B_1**4
-        * (
-            (261 / 128) * B_2**2 * C_3**2
-            + (9 / 16) * C_1 * C_3
-            + (3 / 16) * C_2**2
-            + (9 / 4) * C_2 * C_3 * x_0
-            + (27 / 8) * C_3**2 * x_0**2
-        )
-        + (29 / 64) * B_1**3 * B_2**3 * C_3**2 * np.cos(3 * dw)
-        + B_1**2
-        * (
-            (261 / 128) * B_2**4 * C_3**2
-            + B_2**2
-            * (
-                (9 / 4) * C_1 * C_3
-                + (3 / 4) * C_2**2
-                + 9 * C_2 * C_3 * x_0
-                + (27 / 2) * C_3**2 * x_0**2
-            )
-            + (1 / 2) * C_0 * C_2
-            + (3 / 2) * C_0 * C_3 * x_0
-            + (3 / 8) * C_1**2
-            + 2 * C_1 * C_2 * x_0
-            + (15 / 4) * C_1 * C_3 * x_0**2
-            + 2 * C_2**2 * x_0**2
-            + (13 / 2) * C_2 * C_3 * x_0**3
-            + (39 / 8) * C_3**2 * x_0**4
-        )
-        + (29 / 128) * B_2**6 * C_3**2
-        + B_2**4
-        * (
-            (9 / 16) * C_1 * C_3
-            + (3 / 16) * C_2**2
-            + (9 / 4) * C_2 * C_3 * x_0
-            + (27 / 8) * C_3**2 * x_0**2
-        )
-        + B_2**2
-        * (
-            (1 / 2) * C_0 * C_2
-            + (3 / 2) * C_0 * C_3 * x_0
-            + (3 / 8) * C_1**2
-            + 2 * C_1 * C_2 * x_0
-            + (15 / 4) * C_1 * C_3 * x_0**2
-            + 2 * C_2**2 * x_0**2
-            + (13 / 2) * C_2 * C_3 * x_0**3
-            + (39 / 8) * C_3**2 * x_0**4
-        )
-        + (1 / 2) * C_0**2
-        + C_0 * C_1 * x_0
-        + C_0 * C_2 * x_0**2
-        + C_0 * C_3 * x_0**3
-        + (1 / 2) * C_1**2 * x_0**2
-        + C_1 * C_2 * x_0**3
-        + C_1 * C_3 * x_0**4
-        + (1 / 2) * C_2**2 * x_0**4
-        + C_2 * C_3 * x_0**5
-        + (1 / 2) * C_3**2 * x_0**6
-        + (
-            (87 / 64) * B_1**4 * B_2**2 * C_3**2
-            + B_1**2
-            * (
-                (87 / 64) * B_2**4 * C_3**2
-                + B_2**2
-                * (
-                    (9 / 8) * C_1 * C_3
-                    + (3 / 8) * C_2**2
-                    + (9 / 2) * C_2 * C_3 * x_0
-                    + (27 / 4) * C_3**2 * x_0**2
-                )
+        return self.model_function(**x)
+    
+    def model_function(
+        self,
+        a_1: float,
+        a_2: float,
+        a_scale: float,
+        tau_1: float,
+        tau_2: float,
+        domega: float,
+        dphi: float,
+        x_offset: float,
+    ):
+        b_2 = a_1 * np.exp(-self.x_data / tau_1)
+        b_1 = a_2 * np.exp(-self.x_data / tau_2)
+        dw = self.x_data * domega + dphi
+        return a_scale * np.sqrt(
+            self.func(
+                B_1=b_1,
+                B_2=b_2,
+                dw=dw,
+                x_0=x_offset,
+                **self.coefficients,
             )
         )
-        * np.cos(2 * dw)
-        + (
-            (87 / 64) * B_1**5 * B_2 * C_3**2
-            + B_1**3
-            * (
-                (261 / 64) * B_2**3 * C_3**2
-                + B_2
-                * (
-                    (9 / 4) * C_1 * C_3
-                    + (3 / 4) * C_2**2
-                    + 9 * C_2 * C_3 * x_0
-                    + (27 / 2) * C_3**2 * x_0**2
-                )
-            )
-            + B_1
-            * (
-                (87 / 64) * B_2**5 * C_3**2
-                + B_2**3
-                * (
-                    (9 / 4) * C_1 * C_3
-                    + (3 / 4) * C_2**2
-                    + 9 * C_2 * C_3 * x_0
-                    + (27 / 2) * C_3**2 * x_0**2
-                )
-                + B_2
-                * (
-                    C_0 * C_2
-                    + 3 * C_0 * C_3 * x_0
-                    + (3 / 4) * C_1**2
-                    + 4 * C_1 * C_2 * x_0
-                    + (15 / 2) * C_1 * C_3 * x_0**2
-                    + 4 * C_2**2 * x_0**2
-                    + 13 * C_2 * C_3 * x_0**3
-                    + (39 / 4) * C_3**2 * x_0**4
-                )
-            )
-        )
-        * np.cos(dw)
-    )
-    (
-        29 * B_1**6 * C_3**2 / 128
-        + B_1**4
-        * (
-            261 * B_2**2 * C_3**2 / 128
-            + 9 * C_1 * C_3 / 16
-            + 3 * C_2**2 / 16
-            + 9 * C_2 * C_3 * x_0 / 4
-            + 27 * C_3**2 * x_0**2 / 8
-        )
-        + 29 * B_1**3 * B_2**3 * C_3**2 * cos(3 * w_1 - 3 * w_2) / 64
-        + B_1**2
-        * (
-            261 * B_2**4 * C_3**2 / 128
-            + B_2**2
-            * (
-                9 * C_1 * C_3 / 4
-                + 3 * C_2**2 / 4
-                + 9 * C_2 * C_3 * x_0
-                + 27 * C_3**2 * x_0**2 / 2
-            )
-            + C_0 * C_2 / 2
-            + 3 * C_0 * C_3 * x_0 / 2
-            + 3 * C_1**2 / 8
-            + 2 * C_1 * C_2 * x_0
-            + 15 * C_1 * C_3 * x_0**2 / 4
-            + 2 * C_2**2 * x_0**2
-            + 13 * C_2 * C_3 * x_0**3 / 2
-            + 39 * C_3**2 * x_0**4 / 8
-        )
-        + 29 * B_2**6 * C_3**2 / 128
-        + B_2**4
-        * (
-            9 * C_1 * C_3 / 16
-            + 3 * C_2**2 / 16
-            + 9 * C_2 * C_3 * x_0 / 4
-            + 27 * C_3**2 * x_0**2 / 8
-        )
-        + B_2**2
-        * (
-            C_0 * C_2 / 2
-            + 3 * C_0 * C_3 * x_0 / 2
-            + 3 * C_1**2 / 8
-            + 2 * C_1 * C_2 * x_0
-            + 15 * C_1 * C_3 * x_0**2 / 4
-            + 2 * C_2**2 * x_0**2
-            + 13 * C_2 * C_3 * x_0**3 / 2
-            + 39 * C_3**2 * x_0**4 / 8
-        )
-        + C_0**2 / 2
-        + C_0 * C_1 * x_0
-        + C_0 * C_2 * x_0**2
-        + C_0 * C_3 * x_0**3
-        + C_1**2 * x_0**2 / 2
-        + C_1 * C_2 * x_0**3
-        + C_1 * C_3 * x_0**4
-        + C_2**2 * x_0**4 / 2
-        + C_2 * C_3 * x_0**5
-        + C_3**2 * x_0**6 / 2
-        + (
-            87 * B_1**4 * B_2**2 * C_3**2 / 64
-            + B_1**2
-            * (
-                87 * B_2**4 * C_3**2 / 64
-                + B_2**2
-                * (
-                    9 * C_1 * C_3 / 8
-                    + 3 * C_2**2 / 8
-                    + 9 * C_2 * C_3 * x_0 / 2
-                    + 27 * C_3**2 * x_0**2 / 4
-                )
-            )
-        )
-        * cos(2 * w_1 - 2 * w_2)
-        + (
-            87 * B_1**5 * B_2 * C_3**2 / 64
-            + B_1**3
-            * (
-                261 * B_2**3 * C_3**2 / 64
-                + B_2
-                * (
-                    9 * C_1 * C_3 / 4
-                    + 3 * C_2**2 / 4
-                    + 9 * C_2 * C_3 * x_0
-                    + 27 * C_3**2 * x_0**2 / 2
-                )
-            )
-            + B_1
-            * (
-                87 * B_2**5 * C_3**2 / 64
-                + B_2**3
-                * (
-                    9 * C_1 * C_3 / 4
-                    + 3 * C_2**2 / 4
-                    + 9 * C_2 * C_3 * x_0
-                    + 27 * C_3**2 * x_0**2 / 2
-                )
-                + B_2
-                * (
-                    C_0 * C_2
-                    + 3 * C_0 * C_3 * x_0
-                    + 3 * C_1**2 / 4
-                    + 4 * C_1 * C_2 * x_0
-                    + 15 * C_1 * C_3 * x_0**2 / 2
-                    + 4 * C_2**2 * x_0**2
-                    + 13 * C_2 * C_3 * x_0**3
-                    + 39 * C_3**2 * x_0**4 / 4
-                )
-            )
-        )
-        * cos(w_1 - w_2)
-    )
-    return output
